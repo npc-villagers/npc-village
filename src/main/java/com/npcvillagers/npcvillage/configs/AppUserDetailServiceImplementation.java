652c3ff8c5a37a2e3612398f1fd39d60aa674ecb
Alex Carr <mralex.carr@yahoo.com>
2023-05-15 15:11:47 -0700

    Revert "Todays Work"
    This reverts commit 7d63ba5857430ebfc57f9949077ae18e663b3a94.

--- src/main/java/com/npcvillagers/npcvillage/configs/AppUserDetailServiceImplementation.java
+++ src/main/java/com/npcvillagers/npcvillage/configs/AppUserDetailServiceImplementation.java
@@ -1,19 +1,2 @@
-package com.npcvillagers.npcvillage.configs;
-
-import com.npcvillagers.npcvillage.repos.AppUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AppUserDetailServiceImplementation implements UserDetailsService {
-    @Autowired
-    AppUserRepository appUserRepo;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return (UserDetails) appUserRepo.findByUsername(username);
-    }
+package com.npcvillagers.npcvillage.configs;public class AppUserDetailServiceImplementation {
 }
--- src/main/java/com/npcvillagers/npcvillage/repos/AppUserRepository.java
+++ src/main/java/com/npcvillagers/npcvillage/repos/AppUserRepository.java
@@ -1,9 +1,4 @@
 package com.npcvillagers.npcvillage.repos;
 
-
-import com.npcvillagers.npcvillage.models.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-    public AppUser findByUsername(String username);
+public interface AppUserRepository {
 }
